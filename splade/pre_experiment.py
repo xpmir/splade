04cfa2529561be1363466d6f91945807b7da7ba3
Benjamin Piwowarski <benjamin@piwowarski.fr>
2026-01-18 15:55:58 +0100

    fix: simplify mocking

--- splade/pre_experiment.py
+++ splade/pre_experiment.py
@@ -1,18 +1,9 @@
 import sys
-
-from experimaestro.experiments import FakeModuleFinder
+from experimaestro.experiments import mock_modules
 
 # Usage
-sys.meta_path.insert(0, FakeModuleFinder(
+mock_modules(
     ['torch', 'lightning_fabric', 'torchmetrics', 'pytorch_lightning', 'huggingface_hub', 'transformers'],
-    decorators=[
-        'torch.compile',
-        'torch.jit.unused',
-        'torch.jit.script',
-        'torch.jit.export',
-        'torch.jit.ignore',
-        'torch.no_grad',
-        'torch.inference_mode',
-    ]
-))
+)
+
 print("[installed fake modules for torch and co]", file=sys.stderr)
